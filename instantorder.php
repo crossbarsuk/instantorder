32b09ce46f951b390fb448048b0872557b08dadc
Mayas HADDAD <mayas_91@hotmail.com>
2013-08-22 12:26:39 +0200

    filled the module, we have a basic prestashop module

--- instantorder.php
+++ instantorder.php
@@ -1,3 +1,35 @@
 <?php
 if (!defined('_PS_VERSION_'))
 	exit;
+Class InstantOrder extends Module{
+	
+	public function __construct(){
+		
+		$this->name = 'instantorder';
+		$this->tab = 'Orders';
+		$this->version = '1.0';
+		$this->author = 'Mayas Haddad';
+		$this->need_instance = 0;
+
+		parent::__construct();
+
+		$this->displayName = $this->l('Instant Order');
+		$this->description = $this->l('A Prestashop new confirmed order hook module.');
+
+	}
+
+	public function install(){
+		
+		if(pareint::install() == false){
+			return false;
+		}
+		return true;
+	}
+
+	public function uninstall(){
+		
+		if (!parent::uninstall())
+		Db::getInstance()->Execute('DELETE FROM `'._DB_PREFIX_.'mymodule`');
+		parent::uninstall();
+	}
+}
